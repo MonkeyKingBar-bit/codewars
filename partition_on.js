6502a1d76544db1a34374c5c1d1e44b46f367ad8
Catherine Veremeychik <kateiiverem@gmail.com>
2020-09-15 11:11:23 +0300

    feat: add solution

---
+++ partition_on.js
@@ -0,0 +1,21 @@
+// partition the items array so that all values for which pred returns true are
+// at the end, returning the index of the first true value
+
+const partitionOn = (pred, items) => {
+    const [ts, fs] = partition(pred, items)
+    const newItems = [...ts, ...fs]
+    changeItmes(items, newItems)
+    return ts.length
+  }
+  
+  
+  const partition = (pred, list) => list.reduce(
+    ([ts, fs], cur) => !pred(cur) ? [[...ts, cur], fs] : [ts, [...fs, cur]],
+    [[], []]
+  )
+  
+  const changeItmes = (items, newItems) => {
+    for (let i = 0; i < items.length; i++) {
+      items[i] = newItems[i]
+    }
+  }
